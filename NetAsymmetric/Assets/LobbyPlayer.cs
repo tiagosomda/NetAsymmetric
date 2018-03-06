e86602ab12e241fd44a1ac4157f18461f264f6a5
Tiago <tsomda@gmail.com>
2018-03-05 23:06:06 -0800

    mobile net helper

--- NetAsymmetric/Assets/LobbyPlayer.cs
+++ NetAsymmetric/Assets/LobbyPlayer.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class LobbyPlayer : NetworkBehaviour {
 
-	// Update is called once per frame
-	void Update () {
+
+    
+    private void Start()
+    {
+        if(Application.platform == RuntimePlatform.Android)
+        {
+            CmdSetPlayerType(PlayerType.Mobile);
+            StartCoroutine(FindReadyBtn());
+        }
+    }
+    // Update is called once per frame
+    void Update () {
 
         if (!isLocalPlayer)
             return;
@@ -27,5 +38,25 @@ public class LobbyPlayer : NetworkBehaviour {
     {
         var lobby = GameObject.FindObjectOfType<NetworkManagerAsymmetric>();
         lobby.SetPlayerTypeLobby(connectionToClient, type);
+    }
+
+    [Command]
+    public void CmdPlayerIsReady()
+    {
+        var lobbyPlayer = GetComponent<NetworkLobbyPlayer>();
+        lobbyPlayer.readyToBegin = true;
+    }
+
+    IEnumerator FindReadyBtn()
+    {
+        MobileNetManagerHelper mobileHelper = null;
+
+        while (mobileHelper == null)
+        {
+            mobileHelper = GameObject.FindObjectOfType<MobileNetManagerHelper>();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        mobileHelper.playerReadyBtn.onClick.AddListener(CmdPlayerIsReady);
     }
 }
---
+++ NetAsymmetric/Assets/MobileNetManagerHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MobileNetManagerHelper : MonoBehaviour {
+
+    public GameObject helperPanel;
+    public InputField ipAddressInputField;
+    public Button connectToHostBtn;
+    public Button playerReadyBtn;
+
+    private NetworkManagerAsymmetric netManager;
+	// Use this for initialization
+	void Start () {
+
+        if(Application.platform == RuntimePlatform.Android)
+        {
+            connectToHostBtn.gameObject.SetActive(false);
+            netManager = GameObject.FindObjectOfType<NetworkManagerAsymmetric>();
+            connectToHostBtn.onClick.AddListener(ConnectBtnHandler);
+            ipAddressInputField.onValueChanged.AddListener(InputChangeHandler);
+        }
+        else
+        {
+            helperPanel.SetActive(false);
+        }
+    }
+
+    void ConnectBtnHandler()
+    {
+        netManager.networkAddress = ipAddressInputField.text;
+        netManager.StartClient();
+    }
+
+    void InputChangeHandler(string value)
+    {
+        connectToHostBtn.gameObject.SetActive(value != "");
+    }
+}
